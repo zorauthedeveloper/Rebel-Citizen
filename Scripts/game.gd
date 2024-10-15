b49911e928cf6c45a25f5e104f1c4b7746fae442
Zorau <101998881+zorauthedeveloper@users.noreply.github.com>
2024-10-15 01:24:59 -0400

    Fall damage

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,11 +1,11 @@
 extends Node3D
-@onready var spinnythingy = $MeshInstance3D7
+@onready var spinnythingy = $CSGMesh3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+var speed : Vector3 = Vector3(0,10,0) 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	spinnythingy.rotation_degrees += speed*delta	
